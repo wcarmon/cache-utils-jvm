bfa044549291f1c9af8f5e16f448ac8f541db872
Wil Carmon <java@wcarmon.com>
2024-06-19 08:58:36 -0400

    updates

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Overview
 
-1. Utilities to simplify usage of [`java.util.Properties`](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Properties.html)
+1. Utilities to simplify usage of Caches
 
 
 # Development
---
+++ src/test/java/io/github/wcarmon/cache/ReadThruRefreshAheadCacheTest.java
@@ -0,0 +1,18 @@
+package io.github.wcarmon.cache;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadThruRefreshAheadCacheTest {
+
+    // TODO: value absent + !bypassCache
+
+    // TODO: value absent + bypassCache
+
+    // TODO: value present + !bypassCache
+
+    // TODO: value present + bypassCache
+
+    // TODO: onCacheHit
+
+    // TODO: onCacheMiss
+}
